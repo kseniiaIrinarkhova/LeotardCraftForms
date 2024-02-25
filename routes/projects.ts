d1d2147106e4bb6bdccbe909a2b2aa5061b5286d
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-24 23:27:21 -0800

    Added get/putch/delete methods for project :id

--- routes/projects.ts
+++ routes/projects.ts
@@ -27,6 +27,40 @@ router.route('/')
 
 /*******************Routes with parameters******/
 
+router.route('/:id')
+    .get((req: Request, res: Response) => {
+        let project: Project = projects.find(p => p.id === Number(req.params.id)) as Project;
+        if (project) return res.status(200).send(project)
+        else return res.status(404).send("Ups, something goes wrong")
+    })
+    .patch((req: Request, res: Response)=>{
+        let project: Project = projects.find(p => p.id === Number(req.params.id)) as Project;
+        //as we store objects in projects array, we have a refferance for this object
+        if (project){
+            let key: keyof Project;
+            //check all keys in object
+            for(key in project)
+            {
+                //if we have this key in request body, chenge object
+                if( key !== "id" && (key in req.body))
+                {
+                    project[key] = req.body[key]; 
+                }
+            }
+            return res.status(200).send(project)
+        } 
+        else return res.status(404).send("Ups, something goes wrong")
+    })
+    .delete((req: Request, res: Response) => {
+        let project: Project = projects.find((p,i) => {
+            if(p.id === Number(req.params.id)){
+                projects.splice(i,1);
+                return true;
+            }}) as Project;
+        if (project) return res.status(200).send(`Project ID=${req.params.id} was deleted`)
+        else return res.status(404).send("Ups, something goes wrong")
+    });
+
 
 
 /***********************************************/
