96f5ed4dc1f4229f55b0d90ea2855fdd45e5be2e
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-25 15:34:50 -0800

    changed routes/rhinestones.ts - added next state with errors, changed response object, added comments

--- routes/rhinestones.ts
+++ routes/rhinestones.ts
@@ -1,46 +1,57 @@
 /*******************Import**********************/
-import express, { Express, NextFunction, Request, Response, Router } from "express";
-import { RhinestonesType, Rhinestone, Project } from '../types/main';
-import { projects } from "../data/projects";
+import express, { NextFunction, Request, Response, Router } from "express";
+import {  Rhinestone} from '../types/main';
 import { rhinestones } from "../data/rhinestones";
 import { isRhinostoneType } from "../src/utilities";
+import { error } from "../src/utilities";
 
 /*******************Main Declarations***********/
 const router: Router = express.Router();
 
 /*******************Routes without parameters***/
 router.route('/')
+//get all rhinestones
     .get((req: Request, res: Response) => {
-        return res.send(rhinestones);
+        return res.send({ data: rhinestones });
     })
-    .post((req: Request, res: Response) => {
-
+    //create rhinestone
+    .post((req: Request, res: Response, next:NextFunction) => {
+//check that all required fields are in body
         if (req.body.type && req.body.size && req.body.color) {
-
-            if (!isRhinostoneType(req.body.type)) res.status(500).send("Rhinestone types should be 'Sew-on', 'HotFix' or 'No-HotFix'");
+//check the type of rhinestones
+            if (!isRhinostoneType(req.body.type)) return next(error(500, "Rhinestone types should be 'Sew-on', 'HotFix' or 'No-HotFix'"));
+            //create Rhinestone object
             const rhinestone: Rhinestone = {
                 "id": rhinestones[rhinestones.length - 1].id + 1,
                 "type": req.body.type,
                 "size": req.body.size,
                 "color": req.body.color
             }
+            //add rhinestone to collections
             rhinestones.push(rhinestone);
-            return res.status(201).send(rhinestone);
+            //send result
+            return res.status(201).send({ data: rhinestone });
         }
-        else return res.status(500).send("Wrong body object for POST request");
+        //send error
+        else return next(error(500, "Wrong body object for POST request"));
     });
 
 
 /*******************Routes with parameters******/
 router.route('/:id')
-    .get((req: Request, res: Response) => {
+//get rhinestone by Id
+    .get((req: Request, res: Response, next: NextFunction) => {
+        //try to find rhinestone by Id
         let stone: Rhinestone = rhinestones.find(s => s.id === Number(req.params.id)) as Rhinestone;
-        if (stone) return res.status(200).send(stone)
-        else return res.status(404).send("Ups, something goes wrong")
+        //return object if found
+        if (stone) return res.status(200).send({ data: stone })
+        //return error
+        else return next(error(404, `Oops, something goes wrong. No rhinestoness with ID = ${req.params.id}`));
     })
-    .patch((req: Request, res: Response) => {
+    //change rhinestone by Id
+    .patch((req: Request, res: Response, next: NextFunction) => {
+        //try to find rhinestone by Id
         let stone: Rhinestone = rhinestones.find(s => s.id === Number(req.params.id)) as Rhinestone;
-        console.log(stone)
         //as we store objects in projects array, we have a refferance for this object
         if (stone) {
             let key: keyof Rhinestone;
@@ -51,19 +62,26 @@ router.route('/:id')
                     stone[key] = req.body[key];
                 }
             }
-            return res.status(200).send(stone)
+            //return changed object
+            return res.status(200).send({data: stone})
         }
-        else return res.status(404).send("Ups, something goes wrong")
+        //return error
+        else return next(error(404, `Oops, something goes wrong. No rhinestoness with ID = ${req.params.id}`));
     })
-    .delete((req: Request, res: Response) => {
+    //delete rhinestone by Id
+    .delete((req: Request, res: Response, next: NextFunction) => {
+        //try to find rhinestone by Id
         let stone: Rhinestone = rhinestones.find((s, i) => {
             if (s.id === Number(req.params.id)) {
+                //if found - delete rhinestone from collection
                 rhinestones.splice(i, 1);
                 return true;
             }
         }) as Rhinestone;
-        if (stone) return res.status(200).send(`Rhinestone ID=${req.params.id} was deleted`)
-        else return res.status(404).send("Ups, something goes wrong")
+        //return confirmation
+        if (stone) return res.status(200).send({ message: `Rhinestone ID=${req.params.id} was deleted` })
+        //return error
+        else return next(error(404, `Oops, something goes wrong. No rhinestoness with ID = ${req.params.id}`));
     });
 
 
